e009ba6fa869f3d6200184215734b02ebaf9b26b
Nikolay <afanasev23@gmail.com>
2024-09-03 22:03:51 +0300

    Add library. Add base class

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
-﻿namespace GeometryApp;
+﻿
+using GeometryLibrary;
+
+namespace GeometryApp;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Rect rext = new Rect(2, 2);
+        Console.WriteLine(rext.GetArea());
     }
 }
